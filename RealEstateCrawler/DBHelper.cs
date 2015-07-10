b7e0c69286794281342bd5e32f61ffe8a721ee2b
Ray Lee <rayleesky@outlook.com>
2015-07-11 07:44:16 +0800

    init commit

---
+++ RealEstateCrawler/DBHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateCrawler
+{
+    class DBHelper
+    {
+        private readonly OleDbConnection _conn;
+        private readonly OleDbCommand _cmd;
+
+        public DBHelper(string filePath)
+        {
+            string connstr = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath}";
+            _conn = new OleDbConnection(connstr);
+            _cmd = new OleDbCommand {Connection = _conn};
+        }
+
+        public int InsertLoupanSummary(string name,int price,string address,string region,string city,string url)
+        {
+            _cmd.CommandText =
+                $"INSERT INTO loupan(LoupanName, Price, Address,Region,City,Url)VALUES('{name}', {price}, '{address}','{region}','{city}','{url}')";
+
+            return _cmd.ExecuteNonQuery();
+        }
+
+        public OleDbDataReader GetLoupanSummaryReader(string city)
+        {
+            _cmd.CommandText =
+                $"SELECT * FROM loupan WHERE City = '{city}'";
+            return _cmd.ExecuteReader();
+        }
+
+        public int InsertHistoryPrice(string loupanID, string date, int maxPrice, int avgPrice, int minPrice,
+            string description)
+        {
+            var cmd = new OleDbCommand(
+                $"INSERT INTO HistoryPrice(LoupanID,RecordDate,MaxPrice,AveragePrice,MinPrice,Description) VALUES('{loupanID}','{date}','{maxPrice}','{avgPrice}','{minPrice}','{description}')",
+                _conn);   
+            return cmd.ExecuteNonQuery();
+        }
+
+        public void OpenConnection()
+        {
+            _conn.Open();
+        }
+
+        public void CloseConnection()
+        {
+            _conn.Close();
+        }
+    }
+}
---
+++ RealEstateCrawler/HttpHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateCrawler
+{
+    class HttpHelper
+    {
+        private readonly HttpWebRequest _request;
+        private HttpWebResponse _response;
+
+        public HttpHelper(string Url)
+        {
+            _request = WebRequest.Create(Url) as HttpWebRequest;
+            if (_request != null)
+            {
+                _request.Method = "GET";
+                _request.ContentType = "application/x-www-form-urlencoded;charset=GB2312";
+            }
+        }
+
+        public Stream GetResponseStream()
+        {
+            _response = _request.GetResponse() as HttpWebResponse;
+            if (_response != null)
+            {
+                Stream myStream = _response.GetResponseStream();
+                if (_response.ContentEncoding.ToLower().Contains("gzip"))
+                {
+                    if (myStream != null)
+                        myStream = new GZipStream(myStream, CompressionMode.Decompress);
+                }
+                return myStream;
+            }
+            return Stream.Null;
+        }
+    }
+}
